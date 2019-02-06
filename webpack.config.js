8af0232fc29976456f540992bc897fb0e1845247
Andrii Zakharchenko <art.pruttov@gmail.com>
2019-02-06 19:05:55 +0200

    typescript, add base config for ts

--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,43 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    app: './src/index.js',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
   },
-  devtool: 'inline-source-map',
-   devServer: {
-     contentBase: './dist'
-   },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new UglifyJsPlugin({sourceMap: true}),
-    new HtmlWebpackPlugin({
-      title: 'Development'
-    })
-  ],
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
   output: {
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
+
+// module.exports = {
+//   mode: 'development',
+//   entry: {
+//     app: './src/index.js',
+//   },
+//   devtool: 'inline-source-map',
+//    devServer: {
+//      contentBase: './dist'
+//    },
+//   plugins: [
+//     new CleanWebpackPlugin(['dist']),
+//     new UglifyJsPlugin({sourceMap: true}),
+//     new HtmlWebpackPlugin({
+//       title: 'Development'
+//     })
+//   ],
+//   output: {
+//     filename: '[name].bundle.js',
+//     path: path.resolve(__dirname, 'dist')
+//   }
+// };
